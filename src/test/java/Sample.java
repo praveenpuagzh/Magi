6d5d804d58d82b32c72a77f58b46c4fd242c45e4
praveenpugazh <praveenpugazh84@gmail.com>
2022-02-22 18:59:03 +0530

    first commit

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -1,7 +1,7 @@
 public class Sample {
 
     public static void main(String[] args) {
-        System.out.println("Hi Makeswari");
+        System.out.println("Hi Makeswari iam from pondicherry");
     }
 
 }
